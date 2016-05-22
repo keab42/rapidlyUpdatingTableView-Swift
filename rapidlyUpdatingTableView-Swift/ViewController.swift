5faabb19083689b7b2db727b6e0caa5addb015fd
Matthew Keable <matthew@palringo.com>
2016-05-22 22:55:29 +0100

    Initial Commit

---
+++ rapidlyUpdatingTableView-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rapidlyUpdatingTableView-Swift
+//
+//  Created by Matthew Keable on 22/05/2016.
+//  Copyright © 2016 Matthew Keable. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
