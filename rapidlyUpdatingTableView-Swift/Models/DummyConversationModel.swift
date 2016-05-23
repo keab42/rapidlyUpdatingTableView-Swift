43af0bf9659c7d507bcaca50bf1edb41b3da9dc2
Matthew Keable <matthew@palringo.com>
2016-05-23 01:16:04 +0100

    Setting up layouts and model

---
+++ rapidlyUpdatingTableView-Swift/Models/DummyConversationModel.swift
@@ -0,0 +1,13 @@
+//
+//  DummyConversationModel.swift
+//  rapidlyUpdatingTableView-Swift
+//
+//  Created by Matthew Keable on 23/05/2016.
+//  Copyright © 2016 Matthew Keable. All rights reserved.
+//
+
+import UIKit
+
+class DummyConversationModel: NSManagedObject {
+
+}
